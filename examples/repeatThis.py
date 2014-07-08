c3146d9c264d4dc623a9305b93e839d6a267ce16
Usuario test <pablohe@smn.gob.ar>
2014-07-08 18:16:28 -0300

    orden y cambio de nombres

--- examples/repeatThis.py
+++ examples/repeatThis.py
@@ -3,15 +3,11 @@
 import glob 
 import os
  
-#vamos a un lugar donde no rompamos nada
-os.chdir("/tmp")
+
+os.chdir("/home/usuario/ctpython/examples/observations/")
  
- 
-#crear 10 archivos con el nombre 1.txt, 2.txt... 
-for element in range(10):
-  bashCommand="touch "+str(element)+".txt"
-  os.system(bashCommand)
- 
- 
-for myFile in: glob.glob("/tmp/*"): 
-  print "archivo: "+str(myFile)
+for file_ in glob.glob("*"):
+    os.system("touch "+file_)
+
+
+
