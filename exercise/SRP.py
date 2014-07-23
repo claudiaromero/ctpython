23122cebcdcbf7758aac5d1c153c9ca0a228c1f8
Usuario test <pablohe@smn.gob.ar>
2014-07-23 17:40:29 -0300

    algunos cambios

---
+++ exercise/SRP.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+  userInput = int(input("Ingrese un número: "))
+  
+  print "Ingresó: "+str(userInput)
+
+
